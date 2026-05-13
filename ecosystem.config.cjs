df3828c19c0ab0989709f754f75091ad18a6ae9e
joeglantern <joeglantern12@gmail.com>
2026-05-13 14:12:22 +0300

    Switch backend to port 3002 (3001 taken by existing app)

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       node_args: '--experimental-vm-modules',
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3001,
+        PORT: 3002,
       },
       error_file: '/var/log/flarehub/error.log',
       out_file:   '/var/log/flarehub/out.log',
